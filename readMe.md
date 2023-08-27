bc6248fc1e142656bc1be71f91b01531ddbfe15a
sonamguptacs <sonamcs3092@gmail.com>
2023-08-27 03:34:56 -0700

    Created a pub sub application

--- readMe.md
+++ readMe.md
@@ -1,3 +1,9 @@
 ### Demonstration of Express.js
 
 - free OSS 
+
+`server.js` simply demonstrates an express.js server
+
+`server2.js` demonstrates a pub sub application that open a socket connection to the client using `socket.io`
+
+`index.html` contains the html body and script for opening connection to express server and showing the message emitted by the server.
--- server2.js
+++ server2.js
@@ -2,9 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors:{origin:'*'}
-});
+const io = require('socket.io')(server);
 
 //index.html
 app.get('/', function (req, res) {
@@ -15,13 +13,12 @@ app.get('/', function (req, res) {
     stream.pipe(res);
 })
 
+server.listen(3000,()=>console.log('listening on port 3000'))
 //sockets
-io.on('connection', function (client) {
-    console.log('connected');
-    client.on('message', (message) => {
-        console.log(message);
-        io.emit('message', message);
+
+io.on('connection', (socket) => {
+    console.log('user connected')
+    socket.on("message", function (msg) {
+        socket.emit('message', msg + 'you are connected');
     })
 })
-
-server.listen(3000,()=>console.log('listening on port 3000'))
